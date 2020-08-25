d2f3520fc8f5ad5ea23cd8993e8579a6a724fd8c
root <root@ip-10-0-2-239.ap-south-1.compute.internal>
2020-08-25 08:42:10 +0530

    staging

---
+++ unitia-staging/start.sh
@@ -0,0 +1,1 @@
+docker-compose up --scale delivery=2  --scale dngencore=3 --scale router1=3 --scale router2=3 --scale router3=3 --scale router4=3  --scale router5=3 --scale router6=3 --scale submission=2 -d
